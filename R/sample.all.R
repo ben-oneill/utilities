7117c2294ec2bdc08e0a3c151a0af1b14b95dc58
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2020-12-22 13:29:06 +1100

    Add files via upload

--- R/sample.all.R
+++ R/sample.all.R
@@ -10,9 +10,10 @@
 #' @param n Number of integers to sample from
 #' @param size Length of the sample vectors
 #' @param replace Logical value; if \code{FALSE} the sampling is without replacement; if \code{TRUE} the sampling is with replacement
+#' @prob Probability vector giving the sampling probability for each element (must be a probability vector with length \code{n})
 #' @return A matrix of all permutations of the elements \code{1, ..., n} (rows of the matrix give the permutations)
 
-sample.all <- function(n, size = n, replace = FALSE) {
+sample.all <- function(n, size = n, replace = FALSE, prob = NULL) {
 
   #Check input n
   if (!is.vector(n))                                   { stop('Error: Input n should be a positive integer') }
@@ -33,15 +34,32 @@ sample.all <- function(n, size = n, replace = FALSE) {
   if (!is.logical(replace))                            { stop('Error: Input replace should be a logical value') }
   if (length(replace) != 1)                            { stop('Error: Input replace should be a logical value') }
 
+  #Check input prob
+  if (!is.null(prob)) {
+    if (!is.vector(prob))                              { stop('Error: Input prob should be a probability vector with length n') }
+    if (length(prob) != n)                             { stop('Error: Input prob should be a probability vector with length n') }
+    if (min(prob) < 0)                                 { stop('Error: Input prob should be a probability vector with length n') }
+    PROB <- TRUE
+    SUM  <- sum(prob)
+    if (SUM != 1) { prob <- prob/SUM } }
+
   #Show all sample vectors with replacement
   if (replace) {
 
     #Create output matrix
     N   <- n^size
     OUT <- matrix(0, nrow = N, ncol = size)
+    rownames(OUT) <- sprintf('Sample[%s]', 1:N)
+    colnames(OUT) <- sprintf('X[%s]', 1:size)
+    if (PROB) { OUT <- cbind(OUT, Probability = 1) }
 
     #Fill in the output matrix
-    for (i in 1:size) { OUT[, i] <- rep(1:n, each = n^(size-i)) } }
+    for (i in 1:size) { OUT[, i] <- rep(1:n, each = n^(size-i)) }
+
+    #Fill in the probabilities
+    if (PROB) {
+    for (i in 1:N) {
+      OUT[i, size+1] <- prod(prob[OUT[i, 1:size]]) } } }
 
   #Show all sample vectors without replacement (permutations)
   if (!replace) {
@@ -52,15 +70,18 @@ sample.all <- function(n, size = n, replace = FALSE) {
     #Create output and index matrices
     N   <- factorial(n)/factorial(n-size)
     OUT <- matrix(0, nrow = N, ncol = size)
-    IND <- matrix(0, nrow = N, ncol = size)
-    for (i in 1:size) { IND[, i] <- rep(1:(n-i+1), each = factorial(n-i)/factorial(n-size)) }
+    rownames(OUT) <- sprintf('Sample[%s]', 1:N)
+    colnames(OUT) <- sprintf('X[%s]', 1:size)
+    if (PROB) { OUT <- cbind(OUT, Probability = 1) }
+    for (i in 1:size)   { OUT[, i] <- rep(1:(n-i+1), each = factorial(n-i)/factorial(n-size)) }
 
     #Fill in the output matrix
     for (i in 1:N) {
       VALS <- 1:n
       for (j in 1:size) {
-        index    <- IND[i,j]
+        index    <- OUT[i,j]
         OUT[i,j] <- VALS[index]
+        if (PROB) { OUT[i, size+1] <- OUT[i, size+1]*prob[OUT[i, j]]/sum(prob[VALS]) }
         VALS     <- VALS[-index] } } }
 
   #Return output
