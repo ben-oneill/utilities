edc29426e2574e6837f5912047cf0b9eab6e9eba
Neal Fultz <nfultz@gmail.com>
2021-01-05 22:18:57 +0000

    fix @param tag

--- R/sample.all.R
+++ R/sample.all.R
@@ -9,7 +9,7 @@
 #' @param n Number of integers to sample from
 #' @param size Length of the sample vectors
 #' @param replace Logical value; if \code{FALSE} the sampling is without replacement; if \code{TRUE} the sampling is with replacement
-#' @prob Probability vector giving the sampling probability for each element (must be a probability vector with length \code{n})
+#' @param prob Probability vector giving the sampling probability for each element (must be a probability vector with length \code{n})
 #' @return A matrix of all permutations of the elements \code{1, ..., n} (rows of the matrix give the permutations)
 
 sample.all <- function(n, size = n, replace = FALSE, prob = NULL) {
