5583ba4c597c6edb6d902e138f6e3d0bd7980fdf
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2021-04-16 14:23:34 +1000

    Add files via upload

--- R/softmax.R
+++ R/softmax.R
@@ -1,46 +1,85 @@
-#' Softmax function
+#' Softmax and inverse-softmax functions
 #'
 #' \code{softmax} returns the value of the softmax function
+#' \code{softmaxinv} returns the value of the inverse-softmax function
 #'
 #' The softmax function is a bijective function that maps a real vector with length \code{m-1} to a probability vector
 #' with length \code{m} with all non-zero probabilities.  The softmax function is useful in a wide range of probability
-#' and statistical applications.
+#' and statistical applications.  The present functions define the softmax function and its inverse.
 #'
 #' @usage \code{softmax}
 #' @param eta A numeric vector input
 #' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
 #' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
 #' @return Value of the softmax function
+#'
+#' @usage \code{softmaxinv}
+#' @param p A probability vector (i.e., numeric vector of non-negative values that sum to one)
+#' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
+#' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
+#' @return Value of the inverse-softmax function
 
-  softmax <- function(eta, gradient = FALSE, hessian = FALSE) {
+softmax <- function(eta, gradient = FALSE, hessian = FALSE) {
 
-    #Compute the softmax function
-    m     <- length(eta)+1
-    DEN   <- matrixStats::logSumExp(c(eta, 0))
-    LSOFT <- c(eta, 0) - DEN
-    SOFT  <- exp(LSOFT)
+  #Compute the softmax function
+  m     <- length(eta)+1
+  DEN   <- matrixStats::logSumExp(c(eta, 0))
+  LSOFT <- c(eta, 0) - DEN
+  SOFT  <- exp(LSOFT)
 
-    #Add the gradient (if required)
-    if (gradient) {
-      D1 <- array(0, dim = c(m, m-1))
-      for (k in 1:m)     {
-        for (i in 1:(m-1)) {
-          D1[k,i] <- ((k == i) - SOFT[k])*SOFT[i] } }
-      attr(SOFT, 'gradient') <- D1 }
+  #Add the gradient (if required)
+  if (gradient) {
+    D1 <- array(0, dim = c(m, m-1))
+    for (k in 1:m)     {
+      for (i in 1:(m-1)) {
+        D1[k,i] <- ((k == i) - SOFT[k])*SOFT[i] } }
+    attr(SOFT, 'gradient') <- D1 }
 
-    #Add the Hessian (if required)
-    if (hessian) {
-      D2 <- array(0, dim = c(m, m-1, m-1))
-      for (k in 1:m)     {
-        for (i in 1:(m-1)) {
-          I1 <- (k == i)
-          for (j in 1:(m-1)) {
-            I2 <- (k == j)
-            I3 <- (i == j)
-            D2[k,i,j] <- I1*I2 - I3*SOFT[k] - (I1+I2)*SOFT[i]*SOFT[j] +
-              2*SOFT[i]*SOFT[j]*SOFT[k] } } }
-      attr(SOFT, 'hessian') <- D2 }
+  #Add the Hessian (if required)
+  if (hessian) {
+    D2 <- array(0, dim = c(m, m-1, m-1))
+    for (k in 1:m)     {
+      for (i in 1:(m-1)) {
+        I1 <- (k == i)
+        for (j in 1:(m-1)) {
+          I2 <- (k == j)
+          I3 <- (i == j)
+          D2[k,i,j] <- I1*I2 - I3*SOFT[k] - (I1+I2)*SOFT[i]*SOFT[j] +
+            2*SOFT[i]*SOFT[j]*SOFT[k] } } }
+    attr(SOFT, 'hessian') <- D2 }
 
-    #Return the output
-    SOFT }
+  #Return the output
+  SOFT }
+
+
+softmaxinv <- function(p, gradient = FALSE, hessian = FALSE) {
+
+  #Compute the inverse-softmax function
+  m <- length(p)
+  if (m > 1) {
+    SOFTINV <- (log(p) - log(p[m]))[1:(m-1)] } else {
+    SOFTINV <- numeric(0) }
+
+  #Add the gradient (if required)
+  if (gradient) {
+    D1 <- array(0, dim = c(m-1, m))
+    if (m > 1) {
+    for (k in 1:(m-1))   {
+      for (i in 1:(m-1)) {
+        D1[k,i] <-  1/p[i] }
+        D1[k,m] <- -1/p[m] } }
+    attr(SOFTINV, 'gradient') <- D1 }
+
+  #Add the Hessian (if required)
+  if (hessian) {
+    D2 <- array(0, dim = c(m-1, m, m))
+    if (m > 1) {
+    for (k in 1:(m-1))   {
+      for (i in 1:(m-1)) {
+        D2[k, i, i] <- -1/p[i]^2 }
+        D2[k, m, m] <-  1/p[m]^2 } }
+    attr(SOFTINV, 'hessian') <- D2 }
+
+  #Return the output
+  SOFTINV }
 
