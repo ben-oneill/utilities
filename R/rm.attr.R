aecbeff282057e1df01c28611dfa322ed0243cc3
Neal Fultz <nfultz@gmail.com>
2021-06-07 13:41:25 -0700

    roxygenise rm.attr

--- R/rm.attr.R
+++ R/rm.attr.R
@@ -8,33 +8,32 @@
 #' you do not want to remove attributes from elements of a list (but still remove attributes from the outer
 #' level) you can set \code{list.levels = 0} to do this..
 #'
-#' @usage \code{rm.attr}
 #' @param object An object to operate on attributes from the object
 #' @param list.levels A non-negative integer specifying the number of levels of lists to apply the removal to
 #' @param protected A character vector containing the names of protected attributes (not to be removed)
 #' @return The object is returned with non-protected attributes removed
-
+#'
+#' @examples
+#' a <- structure(list(structure(1, x=2, names=3), list(0, structure(3, x=4, names=5))), x=3, names = 4)
+#' str(rm.attr(a, 1))
 rm.attr <- function(object, list.levels = Inf,
                     protected = c('class', 'dim', 'names', 'dimnames', 'rownames', 'colnames')) {
 
   #Check inputs
-  if (!is.numeric(list.levels))                     stop('Error: Input list.levels should be a non-negative integer')
-  if (length(list.levels) != 1)                     stop('Error: Input list.levels should be a single non-negative integer')
-  if (list.levels == Inf) { LL <- Inf } else { LL <- as.integer(list.levels) }
-  if (LL != list.levels)                            stop('Error: Input list.levels should be a non-negative integer')
-  if (LL < 0)                                       stop('Error: Input list.levels should be a non-negative integer')
-  if (!is.character(protected))                     stop('Error: Input protected should be a character vector')
+  if (!is.numeric(list.levels))             stop('Error: Input list.levels should be a single non-negative integer')
+  if (length(list.levels) != 1)             stop('Error: Input list.levels should be a single non-negative integer')
+  if (list.levels < 0)                      stop('Error: Input list.levels should be a single non-negative integer')
+  if (!is.character(protected))             stop('Error: Input protected should be a character vector')
 
   #Remove unprotected attributes from object
   ATTR.NAMES <- names(attributes(object))
-  REMOVE     <- (!(ATTR.NAMES %in% protected))
   OUT <- object
-  for(i in ATTR.NAMES[REMOVE]) { attr(OUT, i) <- NULL }
 
-  #Remove unprotected attributes
-  if (LL > 0) {
-    if (is.list(OUT)) {
-      for (i in 1:length(OUT)) { OUT[[i]] <- rm.attr(OUT[[i]], list.levels = LL - 1, protected = protected) } } }
+  attributes(OUT)[setdiff(ATTR.NAMES, protected)] <- NULL
+
+  #Recursively remove unprotected attributes from elements
+  if (list.levels > 0 && is.list(OUT)) {
+      OUT[] <- lapply(OUT, rm.attr, list.levels = list.levels - 1, protected=protected) }
 
   #Return output
   OUT }
