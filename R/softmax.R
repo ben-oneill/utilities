40db62cdb516f3501b229d9cd1d841f8ff5cedfa
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2022-04-30 09:30:55 +1000

    Update softmax.R to add logsoftmax
    Add logsoftmax and logsoftmaxinv (its inverse function).

--- R/softmax.R
+++ R/softmax.R
@@ -1,22 +1,48 @@
-#' Softmax and inverse-softmax functions
+#' Softmax and logsoftmax functions and their inverse functions
 #'
-#' \code{softmax} returns the value of the softmax function.
-#' \code{softmaxinv} returns the value of the inverse-softmax function.
+#' \code{softmax} returns the value of the softmax function
+#' \code{softmaxinv} returns the value of the inverse-softmax function
+#' \code{logsoftmax} returns the value of the logsoftmax function
+#' \code{logsoftmaxinv} returns the value of the inverse-logsoftmax function
 #'
 #' The softmax function is a bijective function that maps a real vector with length \code{m-1} to a probability vector
 #' with length \code{m} with all non-zero probabilities.  The softmax function is useful in a wide range of probability
 #' and statistical applications.  The present functions define the softmax function and its inverse, both with a tuning
-#' parameter.
+#' parameter.  It also defines the log-softmax function and its inverse, both with a tuning parameter.
 #'
+#' @usage \code{softmax}
 #' @param eta A numeric vector input
 #' @param lambda Tuning parameter (a single positive value)
 #' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
 #' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
 #' @return Value of the softmax function
 #'
+#' @usage \code{softmaxinv}
+#' @param p A probability vector (i.e., numeric vector of non-negative values that sum to one)
+#' @param lambda Tuning parameter (a single positive value)
+#' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
+#' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
+#' @return Value of the inverse-softmax function
+#'
+#' @usage \code{logsoftmax}
+#' @param eta A numeric vector input
+#' @param lambda Tuning parameter (a single positive value)
+#' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
+#' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
+#' @return Value of the logsoftmax function
+#'
+#' @usage \code{logsoftmaxinv}
+#' @param l A log-probability vector (i.e., numeric vector of non-positive values that logsum to zero)
+#' @param lambda Tuning parameter (a single positive value)
+#' @param gradient Logical; if \code{TRUE} the output will include a \code{'gradient'} attribute
+#' @param hessian Logical; if \code{TRUE} the output will include a \code{'hessian'} attribute
+#' @return Value of the inverse-logsoftmax function
+#'
 #' @examples
 #' softmax(5:7)
 #' softmaxinv(softmax(5:7))
+#' logsoftmax(5:7)
+#' logsoftmaxinv(logsoftmax(5:7))
 
 softmax <- function(eta, lambda = 1, gradient = FALSE, hessian = FALSE) {
 
@@ -51,8 +77,7 @@ softmax <- function(eta, lambda = 1, gradient = FALSE, hessian = FALSE) {
   #Return the output
   SOFT }
 
-#' @param p A probability vector (i.e., numeric vector of non-negative values that sum to one)
-#' @rdname softmax
+
 softmaxinv <- function(p, lambda = 1, gradient = FALSE, hessian = FALSE) {
 
   #Compute the inverse-softmax function
@@ -83,4 +108,61 @@ softmaxinv <- function(p, lambda = 1, gradient = FALSE, hessian = FALSE) {
 
   #Return the output
   SOFTINV }
+
+
+logsoftmax <- function(eta, lambda = 1, gradient = FALSE, hessian = FALSE) {
+  
+  stopifnot(requireNamespace("matrixStats", quietly = TRUE))
+
+  #Compute the logsoftmax function
+  m     <- length(eta)+1
+  DEN   <- matrixStats::logSumExp(c(lambda*eta, 0))
+  LSOFT <- c(lambda*eta, 0) - DEN
+  SOFT  <- exp(LSOFT)
+
+  #Add the gradient (if required)
+  if (gradient) {
+    D1 <- array(0, dim = c(m, m-1))
+    for (k in 1:m)     {
+      for (i in 1:(m-1)) {
+        D1[k,i] <- ((k == i) - SOFT[i]) } }
+    attr(LSOFT, 'gradient') <- lambda*D1 }
+
+  #Add the Hessian (if required)
+  if (hessian) {
+    D2 <- array(0, dim = c(m, m-1, m-1))
+    for (k in 1:m)     {
+      for (i in 1:(m-1)) {
+        for (j in 1:(m-1)) {
+          D2[k,i,j] <- -SOFT[i]*(1-SOFT[j]) } } }
+    attr(LSOFT, 'hessian') <- (lambda^2)*D2 }
+
+  #Return the output
+  LSOFT }
+
+
+logsoftmaxinv <- function(l, lambda = 1, gradient = FALSE, hessian = FALSE) {
+
+  #Compute the inverse-softmax function
+  m <- length(l)
+  if (m > 1) {
+    LSOFTINV <- (l - l[m])[1:(m-1)]/lambda } else { LSOFTINV <- numeric(0) }
+
+  #Add the gradient (if required)
+  if (gradient) {
+    D1 <- array(0, dim = c(m-1, m))
+    if (m > 1) {
+      for (k in 1:(m-1))   {
+        for (i in 1:(m-1)) {
+          D1[k,i] <- 1 }
+        D1[k,m] <- -1 } }
+    attr(LSOFTINV, 'gradient') <- D1/lambda }
+
+  #Add the Hessian (if required)
+  if (hessian) {
+    D2 <- array(0, dim = c(m-1, m, m))
+    attr(LSOFTINV, 'hessian') <- D2 }
+
+  #Return the output
+  LSOFTINV }
 
