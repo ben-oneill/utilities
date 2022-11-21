dd758db4cda12447257bfa6699ac4a8048e0a1e0
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2022-11-21 16:30:56 +1100

    Add files via upload

--- R/benford.R
+++ R/benford.R
@@ -127,7 +127,7 @@ benford <- function(x, base = 10, no.digits = 1,
   FTABLE$Proportion   <- FTABLE$Frequency/N
   FTABLE$Benford.Freq <- N*FTABLE$Benford.Probs
   FTABLE$Digits       <- factor(FTABLE$Digits, levels = FTABLE$Digits)
-  if (no.digits == 1) { names(FTABLE)[1] <- 'Digit' }
+  if (DD == 1) { names(FTABLE)[1] <- 'Digit' }
 
   #Add confidence intervals (using Wilson score method)
   for (i in 1:nrow(FTABLE)) {
@@ -136,6 +136,40 @@ benford <- function(x, base = 10, no.digits = 1,
     FTABLE$Upper[i] <- N*max(CONF.INT) }
   names(FTABLE)[4] <- paste0('Lower[', conf.level, ']')
   names(FTABLE)[5] <- paste0('Upper[', conf.level, ']')
+
+  #Generate Benford data (full)
+  TABLE.ORD  <- table(ORDER)
+  ORDER.ALL  <- as.integer(names(TABLE.ORD))
+  ORDER.FREQ <- unname(c(TABLE.ORD))
+  FTABLE2 <- data.frame(Digits = rep(VALSTRING, times = length(ORDER.ALL)),
+                        Order  = rep(ORDER.ALL, each  = length(VALSTRING)),
+                        Number = rep(VALNUMBER, times = length(ORDER.ALL)),
+                        Total  = rep(ORDER.FREQ, each = length(VALSTRING)),
+                        Frequency = 0, Benford.Freq = 0,
+                        Lower = 0, Upper = 1,
+                        Proportion = 0, Benford.Probs = 0)
+  for (i in 1:nrow(FTABLE2)) {
+    DIG <- FTABLE2$Digits[i]
+    ORD <- FTABLE2$Order[i]
+    NUM <- FTABLE2$Number[i]
+    TOT <- FTABLE2$Total[i]
+    FTABLE2$Frequency[i]     <- sum((DATA$Digit.String[!DATA$Remove] == DIG)*(DATA$Order[!DATA$Remove] == ORD))
+    FTABLE2$Benford.Probs[i] <- log((1+1/NUM), base = BASE)
+    FTABLE2$Proportion[i]    <- FTABLE2$Frequency[i]/TOT
+    FTABLE2$Benford.Freq[i]  <- TOT*FTABLE2$Benford.Probs[i] }
+  FTABLE2$Digits       <- factor(FTABLE2$Digits, levels = VALSTRING)
+  if (no.digits == 1) { names(FTABLE2)[1] <- 'Digit' }
+
+  #Add confidence intervals (using Wilson score method)
+  for (i in 1:nrow(FTABLE2)) {
+    TOT <- FTABLE2$Total[i]
+    CONF.INT <- stat.extend::CONF.prop(alpha = 1-conf.level, n = TOT,
+                                       sample.prop = FTABLE2$Proportion[i])
+    FTABLE2$Lower[i] <- TOT*min(CONF.INT)
+    FTABLE2$Upper[i] <- TOT*max(CONF.INT) }
+  names(FTABLE2)[7] <- paste0('Lower[', conf.level, ']')
+  names(FTABLE2)[8] <- paste0('Upper[', conf.level, ']')
+  FTABLE2 <- FTABLE2[, -(3:4)]
 
   #Run chi-squared test
   SIMULATE <- FALSE
@@ -148,10 +182,29 @@ benford <- function(x, base = 10, no.digits = 1,
   #Generate output
   OUT  <- list(parameters = list(base = BASE, no.digits = DD,
                                  remove.duplicates = remove.duplicates, conf.level = conf.level),
-               data.table = DATA, frequency.table = FTABLE,
+               data.table = DATA, frequency.table = FTABLE, frequency.table.full = FTABLE2,
                test = list(chisq.stat = c(TEST$statistic), p.value = c(TEST$p.value),
                            simulated = SIMULATE, simulations = simulations))
   class(OUT) <- 'benford'
+
+  #Return output
+  OUT }
+
+
+benford.data <- function(object) {
+
+  #Check object class
+  if (!('benford' %in% class(object)))       stop('Error: This plot method is for objects of class \'benford\'')
+
+  #Extract information
+  DATA   <- object$data.table
+  REMOVE <- DATA$Remove
+  MM     <- ncol(DATA)
+  BASE   <- object$parameters$base
+
+  #Generate output
+  OUT <- DATA[!REMOVE, c(1, 3:(MM-1))]
+  attributes(OUT)$base <- BASE
 
   #Return output
   OUT }
@@ -209,7 +262,7 @@ print.benford <- function(object, max.rows = 20) {
   cat('\n') }
 
 
-plot.benford <- function(object, conf.level = NULL,
+plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.legend = FALSE,
                          colour.density = 'blue', colour.bar = 'blue', colour.point = 'green') {
 
   #Check object class
@@ -222,6 +275,16 @@ plot.benford <- function(object, conf.level = NULL,
     if (length(conf.level) != 1)             stop('Error: Input conf.level should be NULL or a single numeric value')
     if (min(conf.level) <= 0)                stop('Error: Input conf.level should be between zero and one')
     if (max(conf.level) >= 1)                stop('Error: Input conf.level should be between zero and one') }
+
+  #Check input by.order
+  if (!is.vector(by.order))                  stop('Error: Input by.order should be a vector')
+  if (!is.logical(by.order))                 stop('Error: Input by.order should be a logical value')
+  if (length(by.order) != 1)                 stop('Error: Input by.order should be a single logical value')
+
+  #Check input order.legend
+  if (!is.vector(order.legend))              stop('Error: Input order.legend should be a vector')
+  if (!is.logical(order.legend))             stop('Error: Input order.legend should be a logical value')
+  if (length(order.legend) != 1)             stop('Error: Input order.legend should be a single logical value')
 
   #Check if required packages are installed
   GGPLOT2   <- requireNamespace('ggplot2',   quietly = TRUE)
@@ -245,19 +308,20 @@ plot.benford <- function(object, conf.level = NULL,
   SIMS     <- object$test$simulations
   DATA     <- object$data.table
   FTABLE   <- object$frequency.table
+  FTABLE2  <- object$frequency.table.full
 
   #Recalculate confidence intervals (if conf.level is specified)
   if (!is.null(conf.level)) {
-  if (conf.level != CL) {
+    if (conf.level != CL) {
 
-    #Remove previous confidence intervals and add new confidence intervals (using Wilson score method)
-    names(FTABLE)[4] <- paste0('Lower[', conf.level, ']')
-    names(FTABLE)[5] <- paste0('Upper[', conf.level, ']')
-    for (i in 1:nrow(FTABLE)) {
-      CONF.INT <- stat.extend::CONF.prop(alpha = 1-conf.level, n = N, sample.prop = FTABLE$Proportion[i])
-      FTABLE[i, 4] <- N*min(CONF.INT)
-      FTABLE[i, 5] <- N*max(CONF.INT) }
-    CL <- conf.level } }
+      #Remove previous confidence intervals and add new confidence intervals (using Wilson score method)
+      names(FTABLE)[4] <- paste0('Lower[', conf.level, ']')
+      names(FTABLE)[5] <- paste0('Upper[', conf.level, ']')
+      for (i in 1:nrow(FTABLE)) {
+        CONF.INT <- stat.extend::CONF.prop(alpha = 1-conf.level, n = N, sample.prop = FTABLE$Proportion[i])
+        FTABLE[i, 4] <- N*min(CONF.INT)
+        FTABLE[i, 5] <- N*max(CONF.INT) }
+      CL <- conf.level } }
 
   #Set plot title and theme
   TITLE <- paste0('Benford Plot')
@@ -279,51 +343,59 @@ plot.benford <- function(object, conf.level = NULL,
                           axis.title.x  = ggplot2::element_text(margin = ggplot2::margin(t = 10, r = 0, b = 0, l = 0)),
                           axis.title.y  = ggplot2::element_text(margin = ggplot2::margin(t = 0, r = 10, b = 0, l = 0)))
 
+  #Set label function for density axis
+  #This function is a variation of scales::math_format which substitutes .base = BASE
+  label_format <- function (expr = .base^.x, format = force) {
+    .x <- NULL
+    quoted <- substitute(expr)
+    subs <- function(x) { do.call("substitute", list(quoted, list(.x = x, .base = BASE))) }
+    function(x) {
+      x <- format(x)
+      ret <- lapply(x, subs)
+      ret <- as.expression(ret)
+      ret[is.na(x)] <- NA
+      names(ret) <- names(x)
+      ret } }
+
   #Create figures
-  #UNRESOLVED PROBLEM: The labeling for the x-axis in Figure 2 is not working correctly
-  #Presently showing the label with the word "BASE" instead of the value BASE
   LOGB <- function(x) { log(x, base = BASE) }
-  names(FTABLE)[c(1, 4:5)] <- c('Digits', 'Lower', 'Upper')
+  names(FTABLE)[c(1, 4:5)]  <- c('Digits', 'Lower', 'Upper')
+  names(FTABLE2)[c(1, 5:6)] <- c('Digits', 'Lower', 'Upper')
   xxx <- seq(from = 1, to = (BASE-1)*BASE^(DD-1), by = BASE^(DD-1))
   LABEL.DIGITS <- rep('', nrow(FTABLE))
   LABEL.DIGITS[xxx] <- as.character(FTABLE[xxx, 1])
-  FIGURE1 <- ggplot2::ggplot(ggplot2::aes(x = Values), data = DATA[!DATA$Remove, ]) +
-               ggplot2::geom_density(fill = colour.density) +
-               ggplot2::scale_x_log10(breaks = scales::trans_breaks("LOGB", function(x) BASE^x),
-                                    #  labels = scales::label_log(base = BASE)) +
-                                       labels = scales::trans_format("LOGB", scales::math_format(BASE^.x))) +
-               THEME + ggplot2::xlab('Value') + ggplot2::ylab('Density')
-  FIGURE2 <- ggplot2::ggplot(ggplot2::aes(x = Digits, y = Frequency), data = FTABLE) +
-               ggplot2::geom_bar(stat = 'identity', fill = colour.bar, position = ggplot2::position_dodge()) +
-               ggplot2::geom_errorbar(ggplot2::aes(ymin = Lower, ymax = Upper), width = 0.2,
-                                      position = ggplot2::position_dodge(0.9)) +
-               ggplot2::geom_point(ggplot2::aes(y = Benford.Freq),
-                                   stat = 'identity', colour = colour.point, size = 3) +
-    ggplot2::scale_x_discrete(labels = LABEL.DIGITS) +
-               THEME + ggplot2::xlab(ifelse(DD == 1, 'Leading Digit', 'Leading Digits')) + ggplot2::ylab('Frequency')
-  FIGURES <- gridExtra::grid.arrange(TITLE, SUBTITLE, FIGURE1, FIGURE2,
-                                     ncol = 1, heights = c(1.4, 3, 14, 14))
 
+  FIGURE1 <- ggplot2::ggplot(ggplot2::aes(x = Values), data = DATA[!DATA$Remove, ]) +
+    ggplot2::geom_density(fill = colour.density) +
+    ggplot2::scale_x_log10(breaks = scales::trans_breaks("LOGB", function(x, base = BASE) base^x),
+                           #  labels = scales::label_log(base = BASE)) +
+                           labels = scales::trans_format("LOGB", label_format(.base^.x))) +
+    THEME + ggplot2::xlab('Value') + ggplot2::ylab('Density')
+  if (!by.order) {
+    FIGURE2 <- ggplot2::ggplot(ggplot2::aes(x = Digits, y = Frequency), data = FTABLE) +
+      ggplot2::geom_bar(stat = 'identity', fill = colour.bar, position = ggplot2::position_dodge()) +
+      ggplot2::geom_errorbar(ggplot2::aes(ymin = Lower, ymax = Upper), width = 0.2,
+                             position = ggplot2::position_dodge(0.9)) +
+      ggplot2::geom_point(ggplot2::aes(y = Benford.Freq),
+                          stat = 'identity', colour = colour.point, size = 3) +
+      ggplot2::scale_x_discrete(labels = LABEL.DIGITS) +
+      THEME + ggplot2::xlab(ifelse(DD == 1, 'Leading Digit', 'Leading Digits')) + ggplot2::ylab('Frequency')
+  } else {
+    FIGURE2 <- ggplot2::ggplot(ggplot2::aes(x = Digits, y = Frequency, fill = Order), data = FTABLE2) +
+      ggplot2::geom_bar(stat = 'identity', position = 'stack') +
+      ggplot2::scale_x_discrete(labels = LABEL.DIGITS) +
+      THEME + ggplot2::xlab(ifelse(DD == 1, 'Leading Digit', 'Leading Digits')) + ggplot2::ylab('Frequency')
+    FIGURE2 <- FIGURE2 + ggplot2::geom_errorbar(ggplot2::aes(ymin = Lower, ymax = Upper, fill = NULL), width = 0.2,
+                                                position = ggplot2::position_dodge(0.9), data = FTABLE) +
+                         ggplot2::geom_point(mapping = ggplot2::aes(x = Digits, y = Benford.Freq, fill = NULL),
+                                             data = FTABLE, stat = 'identity', colour = colour.point, size = 3) +
+                         ggplot2::scale_fill_gradient2(low = 'black', mid = colour.bar, high = 'lightgrey') +
+                         ggplot2::theme(legend.position = 'bottom') +
+                         ggplot2::labs(fill = 'Order-of-Magnitude\n') +
+                         { if (!order.legend) ggplot2::theme(legend.position = 'none') } }
+  FIGURES <- gridExtra::grid.arrange(TITLE, SUBTITLE, FIGURE1, FIGURE2,
+                                     ncol = 1, heights = c(1.4, 3, 14, 14+4*by.order*order.legend))
 
   #Return plot
   FIGURES }
-
-
-benford.data <- function(object) {
-
-  #Check object class
-  if (!('benford' %in% class(object)))       stop('Error: This plot method is for objects of class \'benford\'')
-
-  #Extract information
-  DATA   <- object$data.table
-  REMOVE <- DATA$Remove
-  MM     <- ncol(DATA)
-  BASE   <- object$parameters$base
-
-  #Generate output
-  OUT <- DATA[!REMOVE, c(1, 3:(MM-1))]
-  attributes(OUT)$base <- BASE
-
-  #Return output
-  OUT }
 
