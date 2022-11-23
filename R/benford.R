2b51d83ed1107620cca907f29abf930a997b4b45
Ben O'Neill <47801098+ben-oneill@users.noreply.github.com>
2022-11-23 23:55:06 +1100

    Add files via upload

--- R/benford.R
+++ R/benford.R
@@ -4,7 +4,8 @@
 #'
 #' It is known that under broad conditions, the leading digits in a set of positive values should follow
 #' Benford's distribution.  This tends to occur when those values have a smooth distribution spanning
-#' several orders of magnitude.  In order to identify unusual numeric patterns one can test the frequencies
+#' several orders of magnitude, though it also occurs in some special cases described in the statistical
+#' literature on the topic.  In order to identify unusual numeric patterns one can test the frequencies
 #' of one or more leading digits in a set of values against the expected outcome under Benford's distribution.
 #' Large deviations from Benford's distribution for values spanning over multiple orders of magnitude can
 #' be indicative of unusual numeric patterns warranting further review (e.g., for use in fraud detection).
@@ -15,21 +16,38 @@
 #' then computes the chi-squared statistic for comparison to the Benford distribution and computes the
 #' corresponding p-value.  The output is a list containing information about the analysis, including a data
 #' table with the leading digits of each value, a frequency table showing the frequency and proportion of
-#' the leading digits, and the results of the chi-squared test against Benford's distribution.
+#' the leading digits, another frequency table showing the frequency and proportion of combinations of
+#' leading digits and orders-of-magnitude, and Pearson's chi-squared test against Benford's distribution.
+#'
+#' In order to accommodate "second-order tests", "third-order tests" and so on, the function allows the
+#' user to first transform the data by taking differences of unique sorted values a given number of times.
+#' The output of the function keeps the original data values and each level of this "diffsort" operation,
+#' using only the last set of values for the Benford analysis.
+#'
+#' **Note:** In some cases (e.g., when taking a high-order test or using data with extreme values) the values
+#' used for the test may hit the limits of the lower and upper bounds of representable floating-point numbers
+#' in the machine.  If this occurs it can lead to serious inaccuracy in the test due to miscalculation of the
+#' leading digits.  In this case the print and plot output for the analysis will give a warning to the user
+#' recommending that they change the minimum/maximum representable numbers in the machine.  It is strongly
+#' recommended that you do not rely on any analysis that gives either of these warnings.
 #'
 #' @usage \code{benford(x, base = 10, no.digits = 1, conf.level = 0.95, remove.duplicates = FALSE, simulate.p.value = TRUE, simulations = 10000)}
 #' @param x A vector of values to analyse
 #' @param base A positive integer representing the base for analysis
 #' @param no.digits A positive integer representing the number of leading digits for analysis
+#' @param diffsort A non-negative integer giving the number of times (if any) to take the sorted-difference of the data
 #' @param conf.level The confidence level for confidence intervals for the probability of leading digits
 #' @param remove.duplicates Logical; if \code{TRUE} then duplicate values are removed before testing
 #' @param simulate.p.value Logical value; if \code{TRUE} the p-value for the chi-squared test is simulated if any frequencies are below five
 #' @param simulations A positive integer representing the number of simulations for the p-value if simulation is used
 #' @return A list of class \code{benford} containing results of the analysis
 
-benford <- function(x, base = 10, no.digits = 1,
+benford <- function(x, base = 10, no.digits = 1, diffsort = 0,
                     conf.level = 0.95, remove.duplicates = FALSE,
                     simulate.p.value = TRUE, simulations = 10000) {
+
+  #Get data name
+  DATA.NAME <- deparse(substitute(x))
 
   #Check input x
   if (!is.vector(x))                     stop('Error: Input x should be a numeric vector')
@@ -41,7 +59,7 @@ benford <- function(x, base = 10, no.digits = 1,
   if (!is.vector(base))                  stop('Error: Input base should be an integer')
   if (!is.numeric(base))                 stop('Error: Input base should be an integer')
   if (length(base) != 1)                 stop('Error: Input base should be an integer')
-  BASE <- as.integer(base)
+  BASE <- floor(base)
   if (base != BASE)                      stop('Error: Input base should be an integer')
   if (base < 2)                          stop('Error: Input base should be at least two')
 
@@ -49,9 +67,17 @@ benford <- function(x, base = 10, no.digits = 1,
   if (!is.vector(no.digits))             stop('Error: Input no.digits should be an integer')
   if (!is.numeric(no.digits))            stop('Error: Input no.digits should be an integer')
   if (length(no.digits) != 1)            stop('Error: Input no.digits should be an integer')
-  DD <- as.integer(no.digits)
+  DD <- floor(no.digits)
   if (no.digits != DD)                   stop('Error: Input no.digits should be an integer')
   if (no.digits < 1)                     stop('Error: Input no.digits should be at least one')
+
+  #Check input diffsort
+  if (!is.vector(diffsort))              stop('Error: Input diffsort should be an integer')
+  if (!is.numeric(diffsort))             stop('Error: Input diffsort should be an integer')
+  if (length(diffsort) != 1)             stop('Error: Input diffsort should be an integer')
+  DIFFSORT <- floor(diffsort)
+  if (diffsort != DIFFSORT)              stop('Error: Input diffsort should be an integer')
+  if (diffsort < 0)                      stop('Error: Input diffsort should be non-negative')
 
   #Check input conf.level
   if (!is.vector(conf.level))            stop('Error: Input conf.level should be a number')
@@ -74,7 +100,7 @@ benford <- function(x, base = 10, no.digits = 1,
   if (!is.vector(simulations))           stop('Error: Input simulations should be an integer')
   if (!is.numeric(simulations))          stop('Error: Input simulations should be an integer')
   if (length(simulations) != 1)          stop('Error: Input simulations should be an integer')
-  SIMS <- as.integer(simulations)
+  SIMS <- floor(simulations)
   if (simulations != SIMS)               stop('Error: Input simulations should be an integer')
   if (simulations < 2000)                stop('Error: Input simulations should be at least 2000')
 
@@ -83,6 +109,22 @@ benford <- function(x, base = 10, no.digits = 1,
     stop('Error: This function requires the stat.extend package') }
 
   #############################################################################################################
+
+  #Apply diffsort (if required)
+  DATA.INITIAL <- vector(diffsort + 1, mode= 'list')
+  DATA.INITIAL[[1]] <- VALUES
+  names(DATA.INITIAL)[[1]] <- 'Data'
+  if (diffsort > 0) {
+    names(DATA.INITIAL)[1+1:diffsort] <- sprintf('Diffsort[%s]', 1:diffsort)
+    for (i in 1:diffsort) {
+      DATA.INITIAL[[i+1]] <- diff(sort(unique(DATA.INITIAL[[i]])), decreasing = TRUE) } }
+  VALUES <- DATA.INITIAL[[diffsort+1]]
+
+  #Determine floating-point warning
+  WARNING.MIN <- FALSE
+  WARNING.MAX <- FALSE
+  if (min(VALUES) == .Machine$double.eps)  { WARNING.MIN <- TRUE }
+  if (max(VALUES) == .Machine$double.xmax) { WARNING.MAX <- TRUE }
 
   #Generate Data Table
   ORDER <- floor(log(VALUES, base = BASE))
@@ -127,6 +169,7 @@ benford <- function(x, base = 10, no.digits = 1,
   FTABLE$Proportion   <- FTABLE$Frequency/N
   FTABLE$Benford.Freq <- N*FTABLE$Benford.Probs
   FTABLE$Digits       <- factor(FTABLE$Digits, levels = FTABLE$Digits)
+  FTABLE$Pearson      <- (FTABLE$Frequency - FTABLE$Benford.Freq)^2/FTABLE$Benford.Freq
   if (DD == 1) { names(FTABLE)[1] <- 'Digit' }
 
   #Add confidence intervals (using Wilson score method)
@@ -157,7 +200,8 @@ benford <- function(x, base = 10, no.digits = 1,
     FTABLE2$Benford.Probs[i] <- log((1+1/NUM), base = BASE)
     FTABLE2$Proportion[i]    <- FTABLE2$Frequency[i]/TOT
     FTABLE2$Benford.Freq[i]  <- TOT*FTABLE2$Benford.Probs[i] }
-  FTABLE2$Digits       <- factor(FTABLE2$Digits, levels = VALSTRING)
+  FTABLE2$Digits  <- factor(FTABLE2$Digits, levels = VALSTRING)
+  FTABLE2$Pearson <- (FTABLE2$Frequency - FTABLE2$Benford.Freq)^2/FTABLE2$Benford.Freq
   if (no.digits == 1) { names(FTABLE2)[1] <- 'Digit' }
 
   #Add confidence intervals (using Wilson score method)
@@ -171,6 +215,10 @@ benford <- function(x, base = 10, no.digits = 1,
   names(FTABLE2)[8] <- paste0('Upper[', conf.level, ']')
   FTABLE2 <- FTABLE2[, -(3:4)]
 
+  #Generate output
+  PTABLE <- FTABLE[order(FTABLE$Pearson, decreasing = TRUE), c(1, 8)]
+  rownames(PTABLE) <- 1:nrow(PTABLE)
+
   #Run chi-squared test
   SIMULATE <- FALSE
   if (simulate.p.value) {
@@ -180,11 +228,16 @@ benford <- function(x, base = 10, no.digits = 1,
                                       simulate.p.value = SIMULATE, B = SIMS))
 
   #Generate output
-  OUT  <- list(parameters = list(base = BASE, no.digits = DD,
+  OUT  <- list(parameters = list(base = BASE, no.digits = DD, diffsort = diffsort,
                                  remove.duplicates = remove.duplicates, conf.level = conf.level),
-               data.table = DATA, frequency.table = FTABLE, frequency.table.full = FTABLE2,
+               data = list(data.name = DATA.NAME, data.input = DATA.INITIAL),
+               data.table = DATA,
+               frequency.table = FTABLE,
+               frequency.table.full = FTABLE2,
+               pearson.table = PTABLE,
                test = list(chisq.stat = c(TEST$statistic), p.value = c(TEST$p.value),
-                           simulated = SIMULATE, simulations = simulations))
+                           simulated = SIMULATE, simulations = simulations),
+               warnings = list(warning.min = WARNING.MIN, warning.max = WARNING.MAX))
   class(OUT) <- 'benford'
 
   #Return output
@@ -224,18 +277,37 @@ print.benford <- function(object, max.rows = 20) {
   if (max.rows < 1)                          stop('Error: Input max.rows should be a positive integer')
 
   #Extract information
+  NAME     <- object$data$data.name
   N        <- sum(!object$data.table$Remove)
   RD       <- object$parameters$remove.duplicates
   DD       <- object$parameters$no.digits
   BASE     <- object$parameters$base
+  DS       <- object$parameters$diffsort
   STAT     <- object$test$chisq.stat
   PVAL     <- object$test$p.value
   SIMULATE <- object$test$simulated
   SIMS     <- object$test$simulations
   FTABLE   <- object$frequency.table
+  PTABLE   <- object$pearson.table
+
+  #Give warnings
+  WARNING.MIN <- object$warnings$warning.min
+  WARNING.MAX <- object$warnings$warning.max
+  if (WARNING.MIN)  {
+    warning(paste0('The smallest value used in this test is at the smallest representable floating-point number\n',
+                   '  This can lead to serious inaccuracy in the test due to miscalculation of the leading digits\n',
+                   '  Recommendation: Reduce .machine$double.eps and recompute to allow accurate calculation of leading digits')) }
+  if (WARNING.MAX) {
+    warning(paste0('The largest value used in this test is at the largest representable floating-point number\n',
+                   '  This can lead to serious inaccuracy in the test due to miscalculation of the leading digits\n',
+                   '  Recommendation: Increase .machine$double.xmax and recompute to allow accurate calculation of leading digits')) }
 
   #Print title
   cat('\n  Benford Analysis\n\n')
+  if (DS == 0) {
+    cat(paste0('Dataset: ', NAME, '\n'))
+  } else {
+    cat(paste0('Dataset: ', NAME, ' (diffsort = ', DS, ')\n')) }
   if (DD == 1) {
     cat(paste0('Analysis of ', N, ' values using base ', BASE, '\n'))
   } else {
@@ -256,14 +328,26 @@ print.benford <- function(object, max.rows = 20) {
                  '--- Consider switching to simulated p-value instead\n')) } }
   cat('\n')
 
-  #Print table
+  #Print frequency table
   cat('------------------------------------------------------\n')
+  cat(' Frequency Table\n\n')
   print(FTABLE[, 1:5], row.names = FALSE, max = 5*max.rows)
+  cat('\n')
+
+  #Print Pearson table
+  PTABLE.PRINT <- PTABLE
+  PTABLE.PRINT$a <- ''
+  PTABLE.PRINT <- PTABLE.PRINT[, c(1, 3, 2)]
+  names(PTABLE.PRINT)[2] <- ' '
+  cat('------------------------------------------------------\n')
+  cat(' Pearson Table\n\n')
+  print(PTABLE.PRINT, row.names = FALSE, max = 5*max.rows)
   cat('\n') }
 
 
 plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.legend = FALSE,
-                         colour.density = 'blue', colour.bar = 'blue', colour.point = 'green') {
+                         colour.density = 'blue', colour.bar = 'blue',
+                         colour.point = 'green', colour.pearson = 'darkgreen') {
 
   #Check object class
   if (!('benford' %in% class(object)))       stop('Error: This plot method is for objects of class \'benford\'')
@@ -309,6 +393,19 @@ plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.lege
   DATA     <- object$data.table
   FTABLE   <- object$frequency.table
   FTABLE2  <- object$frequency.table.full
+  PTABLE   <- object$pearson.table
+
+  #Give warnings
+  WARNING.MIN <- object$warnings$warning.min
+  WARNING.MAX <- object$warnings$warning.max
+  if (WARNING.MIN)  {
+    warning(paste0('The smallest value used in this test is at the smallest representable floating-point number\n',
+                   '  This can lead to serious inaccuracy in the test due to miscalculation of the leading digits\n',
+                   '  Recommendation: Reduce .machine$double.eps and recompute to allow accurate calculation of leading digits')) }
+  if (WARNING.MAX) {
+    warning(paste0('The largest value used in this test is at the largest representable floating-point number\n',
+                   '  This can lead to serious inaccuracy in the test due to miscalculation of the leading digits\n',
+                   '  Recommendation: Increase .machine$double.xmax and recompute to allow accurate calculation of leading digits')) }
 
   #Recalculate confidence intervals (if conf.level is specified)
   if (!is.null(conf.level)) {
@@ -327,13 +424,13 @@ plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.lege
   TITLE <- paste0('Benford Plot')
   if (DD == 1) {
     SUBTITLE <- paste0('Analysis of ', N, ' values with base ', BASE, '\n',
-                       'Chi-Sq statistic = ', format(round(STAT, 4), nsmall = 4), ' (', nrow(FTABLE)-1, ' DF), p-value = ', format(round(PVAL, 4), nsmall = 4), '\n',
+                       'Pearson Statistic = ', format(round(STAT, 4), nsmall = 4), ' (', nrow(FTABLE)-1, ' DF), p-value = ', format(round(PVAL, 4), nsmall = 4), '\n',
                        '(Points show expected frequency; Error bars show ',
                        format(100*CL, nsmall = 0), '% confidence intervals)')
   } else {
     SUBTITLE <- paste0('Analysis of ', N, ' values using ', DD, ifelse(DD == 1, ' digit ', ' digits '),
                        'with base ', BASE, '\n',
-                       'Chi-Sq statistic = ', format(round(STAT, 4), nsmall = 4), ' (', nrow(FTABLE)-1, ' DF), p-value = ', format(round(PVAL, 4), nsmall = 4), '\n',
+                       'Pearson Statistic = ', format(round(STAT, 4), nsmall = 4), ' (', nrow(FTABLE)-1, ' DF), p-value = ', format(round(PVAL, 4), nsmall = 4), '\n',
                        '(Points show expected frequency; Error bars show ',
                        format(100*CL, nsmall = 0), '% confidence intervals)') }
   TITLE    <- grid::textGrob(TITLE,    gp = grid::gpar(fontsize = 14, fontface = 'bold'))
@@ -357,7 +454,7 @@ plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.lege
       names(ret) <- names(x)
       ret } }
 
-  #Create figures
+  #Preliminary changes for figures
   LOGB <- function(x) { log(x, base = BASE) }
   names(FTABLE)[c(1, 4:5)]  <- c('Digits', 'Lower', 'Upper')
   names(FTABLE2)[c(1, 5:6)] <- c('Digits', 'Lower', 'Upper')
@@ -365,12 +462,14 @@ plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.lege
   LABEL.DIGITS <- rep('', nrow(FTABLE))
   LABEL.DIGITS[xxx] <- as.character(FTABLE[xxx, 1])
 
+  #Generate Figure 1 - Density Plot (logarithmic scale)
   FIGURE1 <- ggplot2::ggplot(ggplot2::aes(x = Values), data = DATA[!DATA$Remove, ]) +
-    ggplot2::geom_density(fill = colour.density) +
-    ggplot2::scale_x_log10(breaks = scales::trans_breaks("LOGB", function(x, base = BASE) base^x),
-                           #  labels = scales::label_log(base = BASE)) +
-                           labels = scales::trans_format("LOGB", label_format(.base^.x))) +
-    THEME + ggplot2::xlab('Value') + ggplot2::ylab('Density')
+             ggplot2::geom_density(fill = colour.density) +
+             ggplot2::scale_x_log10(breaks = scales::trans_breaks("LOGB", function(x, base = BASE) base^x),
+                                    labels = scales::trans_format("LOGB", label_format(.base^.x))) +
+             THEME + ggplot2::xlab('Value') + ggplot2::ylab('Density')
+
+  #Generate Figure 2 - Frequency Plot
   if (!by.order) {
     FIGURE2 <- ggplot2::ggplot(ggplot2::aes(x = Digits, y = Frequency), data = FTABLE) +
       ggplot2::geom_bar(stat = 'identity', fill = colour.bar, position = ggplot2::position_dodge()) +
@@ -392,9 +491,28 @@ plot.benford <- function(object, conf.level = NULL, by.order = FALSE, order.lege
                          ggplot2::scale_fill_gradient2(low = 'black', mid = colour.bar, high = 'lightgrey') +
                          ggplot2::theme(legend.position = 'bottom') +
                          ggplot2::labs(fill = 'Order-of-Magnitude\n') +
-                         { if (!order.legend) ggplot2::theme(legend.position = 'none') } }
-  FIGURES <- gridExtra::grid.arrange(TITLE, SUBTITLE, FIGURE1, FIGURE2,
-                                     ncol = 1, heights = c(1.4, 3, 14, 14+4*by.order*order.legend))
+      { if (!order.legend) ggplot2::theme(legend.position = 'none') } }
+
+  #Generate Figure 3 - Pearson Statistic
+  names(PTABLE)[1] <- 'Digits'
+  FIGURE3 <- ggplot2::ggplot(ggplot2::aes(x = Digits, y = Pearson), data = PTABLE) +
+             ggplot2::geom_bar(stat = 'identity', fill = colour.pearson, position = ggplot2::position_dodge()) +
+             ggplot2::scale_y_continuous(limits = c(0, 1.2*max(PTABLE$Pearson))) +
+             THEME +
+             ggplot2::xlab(ifelse(DD == 1, 'Leading Digit', 'Leading Digits')) +
+             ggplot2::ylab('Pearson Statistic')
+
+  #Combine figures into single plot
+  #Line up vertical axes in combined plot
+  G1 <- ggplot2::ggplotGrob(FIGURE1)
+  G2 <- ggplot2::ggplotGrob(FIGURE2)
+  G3 <- ggplot2::ggplotGrob(FIGURE3)
+  MAXWIDTH <- grid::unit.pmax(G1$widths[2:5], G2$widths[2:5], G3$widths[2:5])
+  G1$widths[2:5] <- as.list(MAXWIDTH)
+  G2$widths[2:5] <- as.list(MAXWIDTH)
+  G3$widths[2:5] <- as.list(MAXWIDTH)
+  FIGURES <- gridExtra::grid.arrange(TITLE, SUBTITLE, G1, G2, G3, ncol = 1,
+                                     heights = c(1.4, 4, 12, 12+4*by.order*order.legend, 12))
 
   #Return plot
   FIGURES }
